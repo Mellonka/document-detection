5ca555f40686f1b98d7e023114cc93fbf5997f90
Mellonka <93040594+Mellonka@users.noreply.github.com>
2023-06-15 04:10:45 +0500

    support pdf and json for download

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import List
-from PIL import ImageDraw, Image, ImageFont, UnidentifiedImageError
-from services import predict, save_or_upload, draw_text
+from PIL import ImageDraw, ImageFont, UnidentifiedImageError
+from services import predict, save_or_upload_image, save_or_upload_json, draw_text, encode_schema, \
+    get_images_and_filenames
 from schemas import ImagePredict
+import uvicorn
+import json
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'))
@@ -18,12 +21,13 @@ async def root(request: Request):
 
 
 async def inference(files: List[UploadFile] = File()):
-    img_predicts = await predict(files)
+    images_and_filenames = [x for x in get_images_and_filenames(files)]
+    img_predicts = await predict(images_and_filenames)
     annotations = []
     font = ImageFont.truetype('arial.ttf', size=27)
-    for img_predict, img_file in zip(img_predicts, files):
-        image = Image.open(img_file.file)
-        img_format = image.format
+    for image_and_filename, img_predict in zip(images_and_filenames, img_predicts):
+        image, filename = image_and_filename
+        img_format = image.format if image.format is not None else 'JPEG'
         image = image.convert('RGB')
         draw = ImageDraw.Draw(image)
         for box in img_predict.bboxes:
@@ -31,24 +35,33 @@ async def inference(files: List[UploadFile] = File()):
             text = f"{box.box_class} {box.conf}"
             x, y = box.left_top[0], box.right_bottom[1]
             draw_text(draw, font, text, x, y)
-        img_predict.link_to_processed_image = await save_or_upload(image, img_file.filename, img_format)
+        img_predict.link_to_processed_image = await save_or_upload_image(image, filename, img_format)
         annotations.append(img_predict)
     return annotations
 
 
 @app.post("/inference-template")
-async def inference_to_template(request: Request, files: List[UploadFile] = File()):
+async def inference_to_template(request: Request, files: List[UploadFile] = File(...)):
     try:
-        images = await inference(files)
+        annotations = await inference(files)
+        annotations_json = json.dumps(annotations, default=encode_schema, indent=4)
+        link_to_file = await save_or_upload_json(annotations_json)
     except UnidentifiedImageError:
-        return templates.TemplateResponse("index.html", {"request": request, "error": "Вы загрузили не изображение!"})
-    return templates.TemplateResponse("index.html", {"request": request, "images": images})
+        return templates.TemplateResponse("index.html", {"request": request,
+                                                         "error": "Вы загрузили не изображение!"})
+    return templates.TemplateResponse("index.html", {"request": request,
+                                                     "annotations_file": link_to_file,
+                                                     "annotations": annotations})
 
 
 @app.post("/inference-json")
-async def inference_to_json(files: List[UploadFile] = File()) -> List[ImagePredict] | dict:
+async def inference_to_json(files: List[UploadFile] = File(...)) -> List[ImagePredict] | dict:
     try:
-        results = await inference(files)
+        annotations = await inference(files)
     except UnidentifiedImageError:
         return {"error": "Вы загрузили не изображения!"}
-    return results
+    return annotations
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app')
--- services.py
+++ services.py
@@ -1,6 +1,6 @@
 from fastapi import UploadFile
 from ultralytics import YOLO
-from typing import List
+from typing import List, Tuple
 from PIL import Image
 from schemas import BBox, ImagePredict
 from config import AWS_S3_BUCKET_NAME, AWS_SETTINGS
@@ -8,13 +8,34 @@ from io import BytesIO
 from typing import Union
 from uuid import uuid4
 import aioboto3
+from pydantic import BaseModel
+import fitz
 
 
-async def predict(files: List[UploadFile]) -> List[ImagePredict]:
+def pdf2images(pdf_file: UploadFile) -> List[Tuple[Image.Image, str]]:
+    pdf_doc = fitz.open("pdf", pdf_file.file.read())
+    zoom = 1
+    mat = fitz.Matrix(zoom, zoom)
+    results = []
+    for i, page in enumerate(pdf_doc, start=1):
+        filename = f'{pdf_file.filename[:-4]}-page-{i}.jpg'
+        pixmap = page.get_pixmap(matrix=mat)
+        yield Image.frombytes("RGB", [pixmap.width, pixmap.height], pixmap.samples), filename
+    return results
+
+
+def get_images_and_filenames(files: List[UploadFile]):
+    for file in files:
+        if file.filename[-4:] == '.pdf':
+            yield from pdf2images(file)
+        else:
+            yield Image.open(file.file), file.filename
+
+
+async def predict(images_and_filenames: List[Tuple[Image.Image, ImagePredict]]) -> List[ImagePredict]:
     model = YOLO('fast.pt')
     predicts = []
-    for file in files:
-        img = Image.open(file.file)
+    for img, filename in images_and_filenames:
         result = model.predict(img, conf=0.20)
         bboxes = []
         for conf, cls, xyxy in zip(result[0].boxes.conf.tolist(), result[0].boxes.cls.tolist(),
@@ -25,7 +46,7 @@ async def predict(files: List[UploadFile]) -> List[ImagePredict]:
                                conf=round(conf, 2),
                                right_bottom=right_bottom,
                                box_class=model.names[cls]))
-        predicts.append(ImagePredict(filename=file.filename,
+        predicts.append(ImagePredict(filename=filename,
                                      bboxes=bboxes))
     return predicts
 
@@ -39,7 +60,7 @@ async def upload_object(file_key: str, file: Union[UploadFile, bytes, BytesIO]):
         return f'https://storage.yandexcloud.net/{AWS_S3_BUCKET_NAME}/{file_key}'
 
 
-async def save_or_upload(image: Image.Image, img_filename: str, img_format):
+async def save_or_upload_image(image: Image.Image, img_filename: str, img_format):
     if AWS_S3_BUCKET_NAME is not None:
         img_extension = img_filename.split(".")[-1]
         file_key = f'{uuid4()}.{img_extension}'
@@ -52,7 +73,26 @@ async def save_or_upload(image: Image.Image, img_filename: str, img_format):
         return path
 
 
+async def save_or_upload_json(content: str):
+    filename: str = f'{uuid4()}.json'
+    if AWS_S3_BUCKET_NAME is not None:
+        return await upload_object(filename, content.encode())
+    else:
+        path = f'static/inference-jsons/{filename}'
+        with open(path, 'w') as w_file:
+            w_file.write(content)
+        return path
+
+
 def draw_text(draw, font, text: str, x: int, y: int):
     text_width, text_height = draw.textsize(text, font)
     draw.rectangle((x - 5, y, x + text_width + 10, y + text_height), fill='#ff0000')
     draw.text((x, y), text, fill='#ffffff', font=font)
+
+
+def encode_schema(obj):
+    if isinstance(obj, BaseModel):
+        return obj.dict()
+    else:
+        type_name = obj.__class__.__name__
+        raise TypeError(f"Object of type '{type_name}' is not JSON serializable")
